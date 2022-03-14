011c4ee208c89d14d7d411680f284d922a0a3c18
SaNNaN-007 <sannantariq0007@gmail.com>
2022-03-14 15:02:40 +0500

    completed video 43

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -18,4 +18,11 @@ class UserStocksController < ApplicationController
 
     end
 
+    def destroy
+        stock = Stock.find(params[:id])
+        user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+        user_stock.destroy
+        flash[:notice] = "Stock #{stock.ticker} Successfully Removed."
+        redirect_to my_portfolio_path
+    end
 end
